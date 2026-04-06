a0c6ea5223d2797867e7da1caa8f4211e450dd21
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-06 12:00:00 +0530

    Daily python practice

--- Abstraction.py
+++ Abstraction.py
@@ -58,3 +58,93 @@ Uses @abstractmethod
 Cannot create object of abstract class
 Child class must implement abstract methods
 '''
+'''
+Problem 3: Create an Abstract Class
+Create an abstract class Shape with:
+* abstract method area()
+* abstract method perimeter()
+Then create:
+* Circle
+* Rectangle
+'''
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius=radius
+    def area(self):
+        return math.pi*self.radius*self.radius
+    def perimeter(self):
+        return 2*math.pi*self.radius
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+    def area(self):
+        return self.length*self.width
+    def perimeter(self):
+        return 2*(self.length+self.width)
+c = Circle(5)
+print("Circle Area:", c.area())
+print("Circle Perimeter:", c.perimeter())
+r = Rectangle(4, 6)
+print("Rectangle Area:", r.area())
+print("Rectangle Perimeter:", r.perimeter())
+
+'''
+Problem 4: Payment System
+Design an abstract class Payment:
+* method: pay(amount)
+Implement:
+* CreditCardPayment
+* UPIPayment
+* NetBankingPayment
+'''
+from abc import ABC, abstractmethod
+class payment(ABC):
+    @abstractmethod
+    def pay(self,amount):
+        pass
+    
+class CreditCardPayment(payment):
+    def pay(self,amount):
+        print(f"Paid {amount} using Credit Card")
+class UPIPayment(payment):
+    def pay(self,amount):
+        print(f"Paid {amount} using UPI")
+class NetBankingPayment(payment):
+    def pay(self,amount):
+        print(f"Paid {amount} using Net Banking")
+p1=CreditCardPayment()
+p1.pay(1000)
+p2=UPIPayment()
+p2.pay(500)
+p3=NetBankingPayment()
+p3.pay(100)
+
+
+# take a class shape as parent class define draw method in it,  child class as circle
+#as usual draw method should be in child class also then implement it as method overriding
+
+class Shape:
+    def draw(self):
+        print("Draw")
+class Circle(Shape):
+    def draw(self):
+        print("Circle")
+s=Shape()
+c=Circle()
+s.draw()
+c.draw()
+    
+    
