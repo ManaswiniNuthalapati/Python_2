0363ab924488858e476186807b609bbbf72f2ece
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-11 12:00:00 +0530

    Daily python practice

--- Abstraction.py
+++ Abstraction.py
@@ -174,3 +174,18 @@ print(e2.salary())
 Create abstract class Animal with method sound().
 Implement Dog and Cat.
 '''
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+a1 = Dog()
+a2 = Cat()
+print(a1.sound())
+print(a2.sound())
