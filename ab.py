9baa8746473c547c81a02c4ad2313fbcbb0d174e
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-20 20:47:06 +0530

    file updated

--- ab.py
+++ ab.py
@@ -17,3 +17,21 @@ class Student:
 s1=Student("Manaswini",19)
 s1.display()
     
+'''
+Question 2 — Constructor
+Create class Employee
+Constructor should accept:
+name
+salary
+Method:
+show_details()
+'''
+class Employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def show_details(self):
+        print("Name:",self.name)
+        print("Salary:",self.salary)
+e1=Employee("Manaswini",50000)
+e1.show_details()
