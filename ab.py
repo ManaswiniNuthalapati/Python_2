d4f517c56faf214510138cf1b61ee8836bb34bcb
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-20 20:44:24 +0530

    file updated

--- ab.py
+++ ab.py
@@ -1,1 +1,19 @@
-from abc import ABC, abstractmethod
+'''
+Create a class Student
+Attributes:
+name
+marks
+Method:
+display()
+Create 2 objects and print details.
+'''
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def display(self):
+        print("Name:",self.name)
+        print("Marks:",self.marks)
+s1=Student("Manaswini",19)
+s1.display()
+    
