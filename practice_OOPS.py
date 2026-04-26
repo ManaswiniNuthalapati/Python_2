2c11e68878bcd23f41186ca6d34b9198dea109bf
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-26 12:00:00 +0530

    Daily python practice

--- practice_OOPS.py
+++ practice_OOPS.py
@@ -3,13 +3,9 @@ class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-
     def display(self):
         print(f"Name: {self.name}, Marks: {self.marks}")
-
-# Objects
 s1 = Student("Manaswini", 90)
 s2 = Student("Sai", 85)
-
 s1.display()
 s2.display()
