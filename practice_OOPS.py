9e6f1d768f0a3e4f1ca881001082e17403454cdb
Manaswini <nuthalapatimanaswini@gmail.com>
2026-04-27 12:00:00 +0530

    Daily python practice

--- practice_OOPS.py
+++ practice_OOPS.py
@@ -1,11 +1,11 @@
 # Create a Student class with name and marks. Print details.
 class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
     def display(self):
         print(f"Name: {self.name}, Marks: {self.marks}")
-s1 = Student("Manaswini", 90)
-s2 = Student("Sai", 85)
+s1=Student("Manaswini",90)
+s2=Student("Sai",85)
 s1.display()
 s2.display()
