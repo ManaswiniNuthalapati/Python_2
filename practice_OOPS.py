47a36b6f393504b59472715918815340701efb34
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-08 12:00:00 +0530

    Daily python practice

--- practice_OOPS.py
+++ practice_OOPS.py
@@ -41,9 +41,9 @@ print(c.add(10,20))
 
 # Create a class Car with brand and model using constructor.
 class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-c1 = Car("Toyota", "Innova")
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+c1=Car("Toyota","Innova")
 print(c1.brand)
 print(c1.model)
