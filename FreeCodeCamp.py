de0715d69ef3f38e747a153fabb424ca91ec9ef9
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-12 22:27:40 +0530

    file added

---
+++ FreeCodeCamp.py
@@ -0,0 +1,17 @@
+'''
+Write a Python program to create a class named MusicalInstrument.
+
+Requirements:
+
+1. Create an __init__ method that initializes:
+    name → name of the instrument
+    instrument_type → family/type of the instrument
+2. Create a method named play() that prints:
+    "The [instrument name] is fun to play!"
+3. Create another method named get_fact() that returns:
+    "The [instrument name] is part of the [instrument type] family of instruments."
+4. Create two objects:
+    instrument_1 with values "Oboe" and "woodwind"
+    instrument_2 with values "Trumpet" and "brass"
+5. Call the play() method using instrument_1.
+6. Print the result of the get_fact() method using instrument_1.'''
