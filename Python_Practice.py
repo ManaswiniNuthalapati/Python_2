287cb7a22966b29b8cbec3802597390254c794be
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-13 12:01:04 +0530

    file added

--- Python_Practice.py
+++ Python_Practice.py
@@ -73,14 +73,14 @@ else:
   print("special characters")
 
 # Electricity bill calculation
-units = int(input())
-bill = 0
-if units <= 100:
-    bill = units * 1.5
-elif units <= 200:
-    bill = (100 * 1.5) + ((units - 100) * 2.5)
-elif units <= 300:
-    bill = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4)
+units=int(input())
+bill=0
+if units<=100:
+    bill=units*1.5
+elif units<=200:
+    bill=(100*1.5)+((units-100)*2.5)
+elif units<=300:
+    bill=(100*1.5)+(100*2.5)+((units-200)*4)
 else:
-    bill = (100 * 1.5) + (100 * 2.5) + (100 * 4) + ((units - 300) * 6)
+    bill=(100+1.5)+(100*2.5)+(100*4)+((units-300)*6)
 print(bill)
