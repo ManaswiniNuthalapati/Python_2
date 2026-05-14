99903634cad5664b2c4fff81c500df4076f05274
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-14 15:09:42 +0530

    file updated

--- Python_Practice.py
+++ Python_Practice.py
@@ -338,4 +338,44 @@ while True:
     break
   greater+=1
     
-    
+'''
+Star Patterns
+*
+**
+***
+****
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(i):
+    print("*",end=" ")
+  print()
+
+'''
+****
+***
+**
+*
+'''
+n=5
+for i in range(n,0,-1):
+  for j in range(i):
+    print("*",end=" ")
+  print()
+
+'''
+   *
+  ***
+ *****
+*******
+'''  
+
+n=5
+for i in range(1,n+1):
+  for j in range(n-i):
+    print(" ",end="")
+  for k in range(2*i-1):
+    print("*",end="")
+  print()
+ 
+   
