619840f1b41c3f770aea0b621b7424038395abc2
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-22 12:00:00 +0530

    Daily python practice

--- leetcode.py
+++ leetcode.py
@@ -85,3 +85,12 @@ def simpleArraySum(ar):
     for i in ar:
         total+=i
     return total
+  
+# StairCase
+def staircase(n):
+    for i in range(1,n+1):
+        for j in range(n-i):
+            print(" ",end="")
+        for k in range(i):
+            print("#",end="")
+        print()
