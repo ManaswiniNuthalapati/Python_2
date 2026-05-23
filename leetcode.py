ad233fe5f0f93e72e6b2db911d98c636b10c5929
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-23 20:36:15 +0530

    file added

--- leetcode.py
+++ leetcode.py
@@ -64,3 +64,17 @@ else:
     n=n//2
   else:
     print(True)
+    
+# FizzBuzz
+n=3
+res=[]
+for i in range(1,n+1):
+  if i%3==0 and i%5==0:
+    res.append("FizzBuzz")
+  elif i%3==0:
+    res.append("Fizz")
+  elif i%5==0:
+    res.append("Buzz")
+  else:
+    res.append(str(i))
+print(res)
