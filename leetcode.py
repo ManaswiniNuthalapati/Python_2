1a975f5064ad1cf3139b4171bc78471b5ee1e896
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-23 18:27:42 +0530

    file added

---
+++ leetcode.py
@@ -0,0 +1,36 @@
+# Palindrome Number
+x=10
+n=0
+temp=n
+while x>0:
+    digit=x%10
+    n=n*10+digit
+    x=x//10
+print(n)
+if temp==n:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+# Plus One
+digits = [1,2,3]
+res=""
+for i in digits:
+  res+=str(i)
+r=int(res)
+re=r+1
+t=[]
+for i in str(re):
+  t.append(int(i))
+print(t)
+
+# Add Digits
+n=38
+while n>=10:
+  total=0
+  while n>0:
+    digit=n%10
+    total+=digit
+    n=n//10
+  n=total
+print(n)
