3c6c63cb094efa82bb6064e318475ca3f031ac8a
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-28 10:00:00 +0530

    Work done on 27 May

--- leetcode.py
+++ leetcode.py
@@ -276,3 +276,11 @@ def staircase(n):
         print()
 
 # Beautiful days at the movies
+def beautifulDays(i, j, k):
+    count=0
+    for num in range(i,j+1):
+        n=str(num)
+        rev=int(n[::-1])
+        if abs((num-rev)%k)==0:
+            count+=1
+    return count
