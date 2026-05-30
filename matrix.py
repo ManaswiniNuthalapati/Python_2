9d7455d48b2efe6815c5e3d36b8295ac345e3c6b
Manaswini <nuthalapatimanaswini@gmail.com>
2026-05-30 15:34:10 +0530

    file added

--- matrix.py
+++ matrix.py
@@ -10,6 +10,14 @@ for i in range(rows):
   mat.append(row)
 print(mat)
 
+# Sum of all numbers in a Matrix
+A=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+total=0
+for i in range(len(A)):
+  for j in range(len(A[0])):
+    total+=A[i][j]
+print(total)
+
 # Sum of two matrix
 A=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 B=[[2, 0, 3], [6, 9, 1], [5, 2, 8]]
