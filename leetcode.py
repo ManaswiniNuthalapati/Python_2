a74ed7712d942e7a12f6fd7010f82c964930f95f
Manaswini <nuthalapatimanaswini@gmail.com>
2026-06-03 10:33:40 +0530

    file

--- leetcode.py
+++ leetcode.py
@@ -337,4 +337,17 @@ class Solution:
                 res.append(key)
         return sorted(res)
 
+# Single Number
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        for key,value in freq.items():
+            if value<2:
+                return key
+
         
